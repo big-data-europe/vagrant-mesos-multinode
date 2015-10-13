4cdc9fbf50db35dc74cbd5547fa9361c175fec5e
Paul Massey <paul.massey@tenforce.com>
2015-10-13 15:30:49 +0200

    Updates to expand requirements.

--- bootstrap-master.sh
+++ bootstrap-master.sh
@@ -6,30 +6,41 @@ IP=$2
 
 export PATH="/vagrant:$PATH"
 
+###############################################################
 # Switch off the automatic updates message
 echo "APT::Periodic::Update-Package-Lists \"0\";" > /etc/apt/apt.conf.d/10periodic
 
 apt-get install -y dkms virtualbox-guest-dkms build-essential
 
-# Now start to setup for building unified views, etc.
+###############################################################
+# Basic setup required.
+
 apt-get install -y openjdk-7-jre openjdk-7-jdk curl
 apt-get install -y git maven bash emacs nano vim firefox
 apt-get install -y openssh-server
+bootstrap-docker.sh
 
 ###############################################################
+
 bootstrap-hadoop-setup.sh $IP
 start-dfs.sh
 start-mapred.sh
 jps
 
 ###############################################################
+# Enforce restarting of masters should it crash or not.
+###############################################################
+
+###############################################################
 bootstrap-mesos-setup.sh
 
+###############################################################
 # Stop all services so the details can be updated safely.
 service zookeeper stop
 service mesos-master stop
 service marathon stop
 
+###############################################################
 # Configure Zookeeper
 mkdir -p /etc/zookeeper/conf
 mkdir -p /var/lib/zookeeper
@@ -38,24 +49,29 @@ echo ${MASTER} > /var/lib/zookeeper/myid
 cat /vagrant/config-files/zoo.cfg >> /etc/zookeeper/conf/zoo.cfg
 cp /vagrant/config-files/zk.cfg /etc/mesos/zk
 
+###############################################################
 # Configure Mesos
 # (eth0 is the ip assigned via NAT - provided IP is given on bridged eth1)
 echo ${IP} | tee /etc/mesos-master/ip
 cp /etc/mesos-master/ip /etc/mesos-master/hostname
 
+###############################################################
 # Configure Marathon
 mkdir -p /etc/marathon/conf
 cp /etc/mesos-master/hostname /etc/marathon/conf
 cp /etc/mesos/zk /etc/marathon/conf/master
 cp /vagrant/config-files/marathon.cfg /etc/marathon/conf/zk
 
+###############################################################
 # Install Chronos (http://master:4400/)
 apt-get -y install chronos
 
+###############################################################
 # Add master pointer to the hosts list
 echo "127.0.0.1    vmmaster${MASTER}" >> /etc/hosts
 echo "${IP}        vmmaster${MASTER}" >> /etc/hosts
 
+###############################################################
 # Switch off the slave setup
 echo "manual" > /etc/init/mesos-slave.override
 
@@ -64,6 +80,7 @@ update-rc.d mesos-master defaults
 update-rc.d marathon defaults
 update-rc.d chronos defaults
 
+###############################################################
 # Setup startup....
 service zookeeper start
 service mesos-master start
--- bootstrap-mesoshdfsnode.sh
+++ bootstrap-mesoshdfsnode.sh
@@ -4,13 +4,13 @@ IP=$2
 
 export PATH="/vagrant:$PATH"
 
-#################################################################
-# Standard System Updates.
-# Now start to setup for building unified views, etc.
+###############################################################
+# Basic setup required.
 
 apt-get install -y openjdk-7-jre openjdk-7-jdk
 apt-get install -y maven bash emacs nano vim
 apt-get install -y openssh-server
+bootstrap-docker.sh
 
 ###############################################################
 pwd
@@ -24,6 +24,7 @@ mkdir -p /etc/mesos-slave
 # Set current IP (assuming set correctly)
 echo ${IP} | tee /etc/mesos-slave/ip
 
+###############################################################
 # Configure zookeeper, etc.
 mkdir -p /etc/zookeeper/conf
 mkdir -p /var/lib/zookeeper
@@ -34,23 +35,28 @@ cat /vagrant/config-files/zoo.cfg >> /etc/zookeeper/conf/zoo.cfg
 cp /vagrant/config-files/zk.cfg /etc/mesos/zk
 cp /vagrant/config-files/marathon.cfg /etc/marathon/conf/zk
 
+###############################################################
 # Allow docker containers
 echo 'docker,mesos' | tee /etc/mesos-slave/containerizers
 # Recommended (http://frankhinek.com/deploy-docker-containers-on-mesos-0-20/)
 echo "5mins" | tee /etc/mesos-slave/executor_registration_timeout
 
+###############################################################
 # Add slave pointer to the hosts list
 echo "127.0.0.1    vmslave${SLAVE}" >> /etc/hosts
 echo "${IP}        vmslave${SLAVE}" >> /etc/hosts
 
+###############################################################
 # Make sure zookeeper not on slaves
 service zookeeper stop
 echo manual | sudo tee /etc/init/zookeeper.override
 
+###############################################################
 # Switch off the master node stuff
 service mesos-master stop
 echo manual > /etc/init/mesos-master.override
 
+###############################################################
 # Switch on the services, etc.
 update-rc.d mesos-slave defaults
 service mesos-slave restart
